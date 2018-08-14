91893dc1f8970fee411855bd6c8a108dba55fd38
Mert Koseoglu <info@mkoseoglu.com>
2018-08-14 03:18:46 +0300

    review and fix colorpalette test

--- src/components/UI/ColorPalette/ColorPalette.test.tsx
+++ src/components/UI/ColorPalette/ColorPalette.test.tsx
@@ -7,13 +7,30 @@ configure({ adapter: new Adapter() });
 
 describe('ColorPalette Component', () => {
   let wrapper: any;
-
-  beforeEach(() => {
-    wrapper = shallow(<ColorPalette customize={true} colors={[]} />);
-  });
-
+  wrapper = shallow(<ColorPalette colors={[]} />);
   it('should render successfully', () => {
     expect(wrapper.exists()).toEqual(true);
     expect(wrapper.type()).toEqual('div');
+  });
+  it('should isColorPicker initially false', () => {
+    expect(wrapper.state().isColorPickerShow).toEqual(false);
+  });
+  describe('when customize prop is true', () => {
+    beforeEach(() => wrapper.setProps({ customize: true }));
+    it('should isColorPicker true', () => {
+      const mockFn = jest.fn();
+      wrapper.setProps({ onClick: mockFn });
+      wrapper.find('.add-color').simulate('click');
+      expect(wrapper.state().isColorPickerShow).toEqual(true);
+    });
+    it('when click apply button', () => {
+      const mockFn = jest.fn();
+      wrapper.setProps({ onClick: mockFn });
+      wrapper.setState({ currentColor: '#000' });
+      wrapper.find('input').simulate('click', { target: { value: 'Apply' } });
+      expect(wrapper.state().currentColor).toEqual('#000');
+      expect(wrapper.state().isColorPickerShow).toEqual(false);
+      expect(wrapper.state().colors).toEqual(['#000']);
+    });
   });
 });
